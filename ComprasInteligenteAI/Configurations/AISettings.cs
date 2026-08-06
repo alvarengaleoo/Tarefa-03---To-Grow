251def2e94fe962852ed798c564a0d0cf0d4ec0d
Leonardo Alvarenga <alvarengaleoo@gmail.com>
2026-08-05 21:45:23 -0300

    adc config inicial IA

---
+++ ComprasInteligenteAI/Configurations/AISettings.cs
@@ -0,0 +1,12 @@
+﻿namespace ComprasInteligenteAI.Configurations;
+
+public class AISettings
+{
+    public string ApiKey { get; set; } = string.Empty;
+
+    public string Model { get; set; } = string.Empty;
+
+    public double Temperature { get; set; }
+
+    public int MaxTokens { get; set; }
+}
--- ComprasInteligenteAI/Program.cs
+++ ComprasInteligenteAI/Program.cs
@@ -1,14 +1,16 @@
+using ComprasInteligenteAI.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configuração da IA
+builder.Services.Configure<AISettings>(
+    builder.Configuration.GetSection("AISettings"));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
